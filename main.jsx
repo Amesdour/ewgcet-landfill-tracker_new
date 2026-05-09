9d1b0f421c8c6405abc9b50ce6c21bbc6d076af1
Amesdour <mesdourabdellah@gmail.com>
2026-05-09 13:09:15 +0100

    Fix import path for landfill-tracker.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../landfill-tracker.jsx'
+import App from './landfill-tracker.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
