fda119bb2a2858a7be4bd68a7e13168ea2bb5b91
Amesdour <mesdourabdellah@gmail.com>
2026-05-09 13:14:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -384,7 +384,7 @@ if (IS_PROD) {
   const distPath = join(__dirname, 'dist');
   if (existsSync(distPath)) {
     app.use(express.static(distPath));
-    app.get('*', (req, res) => res.sendFile(join(distPath, 'index.html')));
+    app.get('*path', (req, res) => res.sendFile(join(distPath, 'index.html')));
   }
 }
 
